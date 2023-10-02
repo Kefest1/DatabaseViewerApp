cc9451fc9af8929a4823caa7b043ad1b38dc38db
SzymonB5 <235831@edu.p.lodz.pl>
2023-10-02 19:30:10 +0200

    Added databse configuration file

---
+++ Database/init.sql
@@ -0,0 +1,25 @@
+DROP TABLE IF EXISTS database_table;
+DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS ownership_details;
+
+CREATE TABLE users (
+    id SERIAL PRIMARY KEY,
+    masterID INTEGER,
+    name VARCHAR(255),
+    lastname VARCHAR(255),
+    password VARCHAR(255),
+    created_at TIMESTAMP
+);
+
+CREATE TABLE database_table (
+    id SERIAL PRIMARY KEY,
+    table_name VARCHAR(255),
+    created_at TIMESTAMP
+);
+
+CREATE TABLE ownership_details (
+    id SERIAL PRIMARY KEY,
+    level INTEGER,
+    user_id INTEGER REFERENCES users(id),
+    database_id INTEGER REFERENCES database_table(id)
+);
